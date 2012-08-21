8eb95ec9bb2f638822f327694b89e6f1101778e2
Joe Bowser <bowserj@apache.org>
2012-08-21 15:11:40 -0700

    Adding simple connectivity check and geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -9,5 +9,24 @@ var app = {
         // note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
         //app.report('deviceready');
+        var networkState = navigator.network.connection.type;
+        if(networkState == Connection.NONE)
+            onlineFailure();
+        else
+            onlineSuccess();
+    },
+    onlineSuccess: function() {
+        this.onlineWatcher = navigator.geolocation.watchPosition(this.geoSuccess, this.geoFailure, {});
+    },
+    onlineFailure: function() {
+        console.log('This failed');
+    },
+    geoSuccess: function(position) {
+        console.log('We have a location');
+        //Decide whether to send or receive
+    },
+    geoFailure: function() {
+        //Not sure what to do here
+        console.log("We failed to get a location, we should do stuff");
     }
 };
