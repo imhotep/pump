b6d91d2877ccbed9bcc129f3f80fa2dc417b1580
purplecabbage <purplecabbage@gmail.com>
2012-08-21 14:49:39 -0700

    cleanup

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,8 @@
 var app = {
     initialize: function() {
-        this.bind();
-    },
-    bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
     },
-    deviceready: function() {
-        // note that this is an event handler so the scope is that of the event
-        // so we need to call app.report(), and not this.report()
-        app.report('deviceready');
-    },
-    report: function(id) { 
+    init: function(id) { 
         console.log("report:" + id);
         // hide the .pending <p> and show the .complete <p>
         document.querySelector('#' + id + ' .pending').className += ' hide';
